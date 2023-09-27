24f631fa6508ead3e87af0dc54992b017b928199
artemsites <info@artemsites.ru>
2023-09-27 13:31:13 +0400

    upd

--- vite/assets/js/calculator.js
+++ vite/assets/js/calculator.js
@@ -5,20 +5,20 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      // Свойство в котором будет состояние Switcher, изначально равное null
+      // 1 Свойство в котором будет состояние Switcher, изначально равное null
       switcher: null,
     }
   },
   methods: {
-    // Метод который будет переключать состояние switcher
+    // 2 Метод который будет переключать состояние switcher
     switcherChange(event) {
-      // По умолчанию в событие передаётся объект события event
+      // 3 По умолчанию в событие передаётся объект события event
       // из которого мы возьмём свойство target 
       // в котором элемент на котором сработало событие
       // и из него возьмём атрибут checked в котором лежит true или false
       // и запишем в свойство состояния switcher
       this.switcher = event.target.checked
-      
+      // 4 И сразу посмотрим в консоли что состояние приложения меняется
       console.log('this.switcher')
       console.log(this.switcher)
     }
